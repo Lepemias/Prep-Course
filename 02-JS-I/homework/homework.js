bddc91b28d9f8f0972bfa4e754413d751ef7857a
Lepemias <jeresegovia@yahoo.com.ar>
2022-06-01 13:32:21 -0300

    Homework 1.1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -211,9 +211,9 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   if (letra.length >= 2) {return 'Dato incorrecto'}
-  else if (!(letra == 'a' || letra == 'e' ||letra == 'i' || letra == 'o' ||letra == 'u')) {
+  else if (!(letra === 'a' || letra === 'e' ||letra === 'i' || letra === 'o' ||letra === 'u')) {
     return 'Dato incorrecto'}
-  else (letra == 'a' || letra == 'e' ||letra == 'i' || letra == 'o' ||letra == 'u') ;{
+  else (letra === 'a' || letra === 'e' ||letra === 'i' || letra === 'o' ||letra === 'u') ;{
     return 'Es vocal' }
 }
 
